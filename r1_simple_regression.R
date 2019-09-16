f83bdb5cedf77a7d23be55e0275193a2c8b06fd6
eijoac <eijoac@hotmail.com>
2019-09-16 17:24:08 -0400

    added data source

--- r1_simple_regression.R
+++ r1_simple_regression.R
@@ -1,10 +1,13 @@
 # 1. Data Import & Manipulation
 
 # load data
+# this is a dataset used in Mroz 1987 (http://unionstats.gsu.edu/9220/Mroz_Econometrica_LaborSupply_1987.pdf)
+# the dataset is also used throughout Wooldridge's text book: Introductory Econometrics: A Modern Approach.
 data_url <- "https://tdmdal.github.io/r-workshop-researchers/data/mroz_1987.csv"
 mroz_1987 <- read.csv(data_url)
 
 # take a look at the structure of the data
+# https://justinmshea.github.io/wooldridge/reference/mroz.html
 str(mroz_1987)
 
 # print the first few rows of the dataset
